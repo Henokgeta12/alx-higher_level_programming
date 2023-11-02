f9f93014b2a820accce0fccaa694b295f8d6cf9b
henokgeta12 <henokasfaw47@icloud.com>
2023-11-02 10:32:11 -0700

    alx

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from sys import argv
+    if argv[0]
+       print("1 argument:")
+       print("1 { }",format(argv[0]))
+   else   
+      for arg1 in range(1,len(argv))
+          print("{ } arguments",format(len(argv)))  
