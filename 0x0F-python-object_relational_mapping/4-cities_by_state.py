efa19638a84219edfd5b15a6851056af4c5640c7
henokgeta12 <henokasfaw47@icloud.com>
2024-02-17 06:57:01 -0800

    make executable

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+   
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],port = 3306, db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities WHERE ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
