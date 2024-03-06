65e17a1b7c3a4509905dfa15e5f9ca411f5fc7c1
Henokgeta12 <136017420+Henokgeta12@users.noreply.github.com>
2024-03-06 09:13:26 +0300

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/3-methods.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-url=$1
-
-methods=$(curl -sI -X OPTIONS "$url" | grep "Allow:" | cut -d' ' -f2-)
-
-echo "HTTP Methods:"
-echo "$methods"
--- 0x10-python-network_0/README.md
+++
@@ -1,1 +0,0 @@
-alx python with network directory
